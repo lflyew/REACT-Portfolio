715242a8ab63f18ffff8afd6c2c2ad1781a2b5be
Latoya Lyew <nursebae@Latoyas-MBP.hsd1.ga.comcast.net>
2022-12-18 21:46:45 -0500

    about me section

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function About() {
+    return (
+        <div className='section' id='about'>
+          <h1 >About Me</h1>
+          <span>Get to know</span>
+          <br /><br />
+          <div className='about'>
+            <div className='about-item'>
+              <img width='100%' src="https://rgonsahn.github.io/robina-week-2-challenge/IMG_0920_Me-too.jpg" alt="" />
+            </div>
+            <div className='about-item-2'>
+                <h2> Latoya Lyew</h2>
+                <p> I am a Master's Degree Registered Nurse turned Techie. I have been a Emergency Room Nurse for 15 years. 
+                I Love to travel and spend time with my daughter! I'm excited to learn and grow in the field of coding! &#128512;</p>
+
+                <b>Phone Number: (770)906-3181</b><br />
+                <b>Email: lflyew@gmail.com</b>
+
+                </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default About
