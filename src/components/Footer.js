d0694fedc04ad8544e7d40bbbfcaf10d7f9a5820
Latoya Lyew <nursebae@Latoyas-MBP.hsd1.ga.comcast.net>
2022-12-18 22:24:32 -0500

    header and footer created

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className='footer'>
+
+        <div className=' footer-section'>
+          <a href="https://www.linkedin.com/in/latoya-lyew/">LinkedIn</a>
+          <a href="https://github.com/lflyew">Github</a>
+
+        </div>
+        <b> Created By: Latoya Lyew</b>
+        </div>
+
+    )
+}
+
+export default Footer
