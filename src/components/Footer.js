d8de4302aa8f36fcf3b5d2e00888b418e5125c57
Latoya Lyew <nursebae@Latoyas-MBP.hsd1.ga.comcast.net>
2022-12-18 22:29:13 -0500

    portfolio started

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ function Footer() {
         <div className=' footer-section'>
           <a href="https://www.linkedin.com/in/latoya-lyew/">LinkedIn</a>
           <a href="https://github.com/lflyew">Github</a>
+          <a href="">Stack Overflow</a>
 
         </div>
         <b> Created By: Latoya Lyew</b>
---
+++ src/components/Portfolio.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from '../images/logo.jpeg'
+
+function Portfolio() {
+    return (
+        <div className='section porto-section' id='portfolio'>
+
+      <br /><br />
+      <h1>Portofolio</h1>
+      <span> Projects </span>
+      <br />
+      <br />
+      <div className='porto-container'>
+        <div className="porto-item">
+        <img src=""
+    )
+}
+
+export default Portfolio
