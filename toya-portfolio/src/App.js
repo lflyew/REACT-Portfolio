bbbd86aa04f62e93b43e01ba51329a2f65b3ca0b
Latoya Lyew <nursebae@Latoyas-MBP.hsd1.ga.comcast.net>
2022-12-18 21:36:02 -0500

    create react app.js file

---
+++ toya-portfolio/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import About from "./components/About"
+import Contact from './components/Contact'
+import Header from './components/Header'
+import Portfolio from './components/Portfolio'
+import Resume from "./components/Resume"
+
+function App() {
+  return <div>
+    <Header />
+    <About />
+    <Portfolio />
+    <Contact />
+    <Resume />
+  </div>
+}
+
+export default App
